cdf989c3442a1a4ea8d8f5474959521af08905c2
Connor Makowski <connorman528@gmail.com>
2023-06-27 09:57:09 -0400

    1.5.0b9: Fix delete for args starting with -- (for django pass through)

--- cave_utils/arguments.py
+++ cave_utils/arguments.py
@@ -16,6 +16,13 @@ class Arguments:
                 skip_next = False
                 continue
             if i.lower().startswith("--"):
+                if idx + 1 >= len(self.passed_args):
+                    self.flags.append(i[2:])
+                    continue
+                else:
+                    if self.passed_args[idx + 1].lower().startswith("-"):
+                        self.flags.append(i[2:])
+                        continue
                 self.kwargs[i[2:]] = self.passed_args[idx + 1]
                 skip_next = True
             elif i.lower().startswith("-"):
@@ -29,18 +36,16 @@ class Arguments:
     def has_flag(self, key):
         return key in self.flags
 
-    def delete(self, key, silent=False):
+    def delete(self, key, silent=False, only_flag=False):
         if key not in self.kwargs and key not in self.flags and key not in self.other:
             if not silent:
                 print(f"Key {key} not found in arguments")
             return
         for idx, i in enumerate(self.passed_args):
-            if i == f"--{key}":
+            if i == f"--{key}" or i == f"-{key}" or i == key:
                 del self.passed_args[idx]
-                del self.passed_args[idx]
-                break
-            if i == f"-{key}" or i == key:
-                del self.passed_args[idx]
+                if key in self.kwargs and not only_flag:
+                    del self.passed_args[idx]
                 break
         self.populate_data()
 
--- test/test_arguments.py
+++ test/test_arguments.py
@@ -7,3 +7,13 @@ print(args.flags)
 print(args.other)
 
 print(args.get_kwarg("test", "default"))
+
+args.delete("test", only_flag=True)
+
+args.delete("abc")
+
+print(args.kwargs)
+print(args.flags)
+print(args.other)
+
+print(args.get_kwarg("test", "default"))
