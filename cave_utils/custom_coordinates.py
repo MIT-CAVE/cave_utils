27c09f3569c89db51e0d592a0b584c3d11e484d8
Anthony Zheng <zhenganthony26@gmail.com>
2025-08-06 16:47:50 -0400

    Handle altitude with CustomCoordinateSystem

--- cave_utils/custom_coordinates.py
+++ cave_utils/custom_coordinates.py
@@ -3,28 +3,31 @@ import type_enforced
 
 class CustomCoordinateSystem():
     @type_enforced.Enforcer
-    def __init__(self, width: float | int, height: float | int):
+    def __init__(self, length: float | int, width: float | int, height: float | int = 10000):
         """
-        Creates a custom Cartesian coordinate system with the origin (0, 0) located at the bottom-left
-        of the map and x and y increasing in value whilst moving right and up, respectively.
+        Creates a custom 2D or 3D Cartesian coordinate system with the origin (0, 0) located at the bottom-left
+        of the map and x and y increasing in value whilst moving right and up along the flat plane, respectively. z
+        increases whilst moving up along the vertical axis.
 
         Arguments:
 
-        * **`width`**: `[float | int]` &rarr; The maximum x value of this coordinate system.
-        * **`height`**: `[float | int]` &rarr; The maximum y value of this coordinate system.
+        * **`length`**: `[float | int]` &rarr; The maximum x value of this coordinate system.
+        * **`width`**: `[float | int]` &rarr; The maximum y value of this coordinate system.
+        * **`height`**: `[float | int]` = `10000` &rarr; The maximum z value of this coordinate system.
 
         Returns:
 
         * `[None]`
         """
+        self.length = length
         self.width = width
         self.height = height
-        self.radius = max(width, height) / (2 * math.pi)
-        self.margin = abs(width - height) / 2
+        self.radius = max(length, width) / (2 * math.pi)
+        self.margin = abs(length - width) / 2
 
-    def convert_to_long_lat(self, coordinates: list[list[float | int]]):
+    def serialize_coordinates(self, coordinates: list[list[float | int]]):
         """
-        Converts (x, y, z) coordinates using this coordinate system to a longitude-latitude-altitude system.
+        Serializes (x, y, z) coordinates in this coordinate system to a longitude-latitude-altitude system.
         Formula adapted from: https://en.wikipedia.org/wiki/Mercator_projection#Derivation
 
         Arguments:
@@ -38,11 +41,18 @@ class CustomCoordinateSystem():
         * `[list[list[float | int]]]` &rarr; The converted coordinates in the format `[[long1,lat1,(possible alt1)],[long2,lat2,(possible alt2)],...]` .
         """
         long_lat_coordinates = []
+        has_altitude = len(coordinates[0]) == 3
+        if has_altitude:
+            scale = 10000 / self.height
+
         for coordinate in coordinates:
             x = coordinate[0]
-            y = coordinate[1] - self.height / 2
+            y = coordinate[1] - self.width / 2
+            if has_altitude: 
+                z = coordinate[2]
+                altitude = z * scale
 
-            if self.height > self.width:
+            if self.width > self.length:
                 x += self.margin
 
             longitude = (x / self.radius) * (180 / math.pi)
@@ -51,7 +61,7 @@ class CustomCoordinateSystem():
             # Y values close to 0 will not display on map 
             if latitude < -85.05 and coordinate[1] >= 0:
                 latitude = -85.05
-            long_lat_coordinates.append([longitude - 180, latitude])
+            long_lat_coordinates.append([longitude - 180, latitude, altitude] if has_altitude else [longitude - 180, latitude])
 
         return long_lat_coordinates
 
@@ -67,8 +77,18 @@ class CustomCoordinateSystem():
 
         * `[dict]` &rarr; The serialized location structure.
         """
-        converted_coordinates = self.convert_to_long_lat(coordinates)
-        return {
+        converted_coordinates = self.serialize_coordinates(coordinates)
+        if len(converted_coordinates[0]) == 2:
+            return {
             "latitude": [[coordinate[1]] for coordinate in converted_coordinates],
             "longitude": [[coordinate[0]] for coordinate in converted_coordinates]
+            }
+        return {
+            "latitude": [[coordinate[1]] for coordinate in converted_coordinates],
+            "longitude": [[coordinate[0]] for coordinate in converted_coordinates],
+            "altitude": [[coordinate[2]] for coordinate in converted_coordinates]
         }
+
+c = CustomCoordinateSystem(14, 14, 14)
+coordinates = [[0, 0, 0], [7, 7, 7], [14, 14, 14]]
+print(c.serialize_nodes(coordinates))
